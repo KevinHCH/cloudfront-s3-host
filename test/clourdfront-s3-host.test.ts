a37ada92da600768183f32bc08c16cf2a92101bc
kevin.herbas <kevin.herbas@infoleads.org>
2023-04-07 10:09:03 +0200

    Initial commit

---
+++ test/clourdfront-s3-host.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ClourdfrontS3Host from '../lib/clourdfront-s3-host-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/clourdfront-s3-host-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ClourdfrontS3Host.ClourdfrontS3HostStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
